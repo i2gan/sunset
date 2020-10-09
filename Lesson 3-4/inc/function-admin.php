5427b50f37a1ea9d7a47211aba5bfd40759961cf
i2gan_github <i2gan0f@gmail.com>
2020-10-09 14:35:48 +0400

    Lesson 5

--- Lesson 3-4/inc/function-admin.php
+++ Lesson 3-4/inc/function-admin.php
@@ -1,4 +1,3 @@
-  
 <?php
 
 /*
@@ -29,8 +28,17 @@ add_action( 'admin_menu', 'sunset_add_admin_page' );
 
 function sunset_custom_settings() {
     register_setting( 'sunset-settings-group', 'first_name' );
+    register_setting( 'sunset-settings-group', 'last_name' );
+    register_setting( 'sunset-settings-group', 'twitter_handler', 'sunset_sanitize_twitter_handler' );
+    register_setting( 'sunset-settings-group', 'facebook_handler' );
+    register_setting( 'sunset-settings-group', 'gplus_handler' );
+    
     add_settings_section( 'sunset-sidebar-options', 'Sidebar Option', 'sunset_sidebar_options', 'alecaddd_sunset');
-    add_settings_field( 'sidebar-name', 'First Name', 'sunset_sidebar_name', 'alecaddd_sunset', 'sunset-sidebar-options');
+    
+    add_settings_field( 'sidebar-name', 'Full Name', 'sunset_sidebar_name', 'alecaddd_sunset', 'sunset-sidebar-options');
+    add_settings_field( 'sidebar-twitter', 'Twitter handler', 'sunset_sidebar_twitter', 'alecaddd_sunset', 'sunset-sidebar-options');
+    add_settings_field( 'sidebar-facebook', 'Facebook handler', 'sunset_sidebar_facebook', 'alecaddd_sunset', 'sunset-sidebar-options');
+    add_settings_field( 'sidebar-gplus', 'Google+ handler', 'sunset_sidebar_gplus', 'alecaddd_sunset', 'sunset-sidebar-options');
 }
 
 function sunset_sidebar_options() {
@@ -39,9 +47,28 @@ function sunset_sidebar_options() {
 
 function sunset_sidebar_name() {
     $firstName = esc_attr( get_option( 'first_name' ) );
-    echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="First Name" />';
+    $lastName = esc_attr( get_option( 'last_name' ) );
+    echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="First Name" /> <input type="text" name="last_name" value="'.$lastName.'" placeholder="Last Name" />';
+}
+function sunset_sidebar_twitter() {
+    $twitter = esc_attr( get_option( 'twitter_handler' ) );
+    echo '<input type="text" name="twitter_handler" value="'.$twitter.'" placeholder="Twitter handler" /><p class="description">Input your Twitter username without the @ character.</p>';
+}
+function sunset_sidebar_facebook() {
+    $facebook = esc_attr( get_option( 'facebook_handler' ) );
+    echo '<input type="text" name="facebook_handler" value="'.$facebook.'" placeholder="Facebook handler" />';
+}
+function sunset_sidebar_gplus() {
+    $gplus = esc_attr( get_option( 'gplus_handler' ) );
+    echo '<input type="text" name="gplus_handler" value="'.$gplus.'" placeholder="Google+ handler" />';
 }
 
+//Sanitization settings
+function sunset_sanitize_twitter_handler( $input ){
+    $output = sanitize_text_field( $input );
+    $output = str_replace('@', '', $output);
+    return $output;
+}
 
 function sunset_theme_create_page() {
     require_once( get_template_directory() . '/inc/templates/sunset-admin.php' );
